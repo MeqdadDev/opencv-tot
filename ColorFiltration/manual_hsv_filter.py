7768ba27f572ee5c85cd2a5500b787633620a9d1
MeqdadDev <meqdad.darweesh@gmail.com>
2023-11-22 20:21:29 +0200

    update manunal hsv color filtration example

--- ColorFiltration/manual_hsv_filter.py
+++ ColorFiltration/manual_hsv_filter.py
@@ -9,14 +9,11 @@ upper_blue = np.array([180, 255, 255])
 
 while True: 
 	_, frame = cap.read() 
-	# It converts the BGR color space of image to HSV color space 
+
 	hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV) 
 	
-	# preparing the mask to overlay
 	mask = cv.inRange(hsv, lower_blue, upper_blue)
 	
-	# The black region in the mask has the value of 0, 
-	# so when multiplied with original image removes all non-blue regions 
 	result = cv.bitwise_and(frame, frame, mask = mask) 
 
 	cv.imshow('Frame', frame) 
