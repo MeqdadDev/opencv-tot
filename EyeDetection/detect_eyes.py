c9d985f8836d73eb352bbb654b3fb0641f115a5c
MeqdadDev <meqdad.darweesh@gmail.com>
2023-12-13 18:55:04 +0200

    upload eye detection code example

---
+++ EyeDetection/detect_eyes.py
@@ -0,0 +1,22 @@
+import cv2 as cv
+cam = cv.VideoCapture(0)
+eye_cascade = cv.CascadeClassifier('resources/haarcascade_eye.xml')
+
+def detect_eyes(img):
+    eyes_frame = img.copy()
+    # Returns the rectangle borders
+    face_rects = eye_cascade.detectMultiScale(eyes_frame)
+    for (x, y, w, h) in face_rects:
+        cv.rectangle(eyes_frame, (x, y), (x+w, y+h), (255, 0, 255), 5)
+    return eyes_frame
+
+while True:
+    _, frame = cam.read()
+    detection = detect_eyes(frame)
+    cv.imshow('Eyes Detection View', detection)
+    k = cv.waitKey(1)
+
+    if k % 256 == 27:
+        break
+
+cam.release()
